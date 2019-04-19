4bbc135c12cf7861baed561704a73635d45b197e
Stanisław Góra <stasgora@gmail.com>
2019-04-19 23:15:20 +0200

    added license headers

--- src/main/java/com/github/stasgora/observetree/ListenerEntry.java
+++ src/main/java/com/github/stasgora/observetree/ListenerEntry.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2019 Stanisław Góra
+ *
+ * This file is part of Observetree library which is released under MIT License.
+ * See LICENSE file or go to http://www.opensource.org/licenses/mit-license.php for full license details.
+ */
+
 package com.github.stasgora.observetree;
 
 import java.util.Objects;
