29af7628522c5e4f9a163f3821cf8679a0b83532
Stanisław Góra <stasgora@gmail.com>
2019-04-21 20:45:17 +0200

    settable observable documentation

--- src/main/java/com/github/stasgora/observetree/SettableObservable.java
+++ src/main/java/com/github/stasgora/observetree/SettableObservable.java
@@ -29,29 +29,64 @@ public class SettableObservable<T extends Observable> extends SettableProperty<T
 
 	private transient Set<ListenerEntry> staticListeners = new TreeSet<>();
 
+	/**
+	 * Constructs a new, empty {@code SettableObservable} with {@code null} {@link #modelValue}
+	 */
 	public SettableObservable() {
 	}
 
+	/**
+	 * Constructs a new {@code SettableObservable} with the specified {@link #modelValue}
+	 * @param modelValue a value to be set
+	 */
 	public SettableObservable(T modelValue) {
 		super(modelValue);
 	}
 
+	/**
+	 * Adds the specified listener to the list of static listeners with the default priority {@link ListenerPriority#NORMAL} (0).
+	 * @param listener element to be added
+	 * @return {@code true} if the listener was successfully added. {@code false} if it was already present
+	 */
 	public boolean addStaticListener(ChangeListener listener) {
 		return add(staticListeners, listener);
 	}
 
+	/**
+	 * Adds the specified listener to the list of static listeners with the specified priority.
+	 * @param listener element to be added
+	 * @param priority priority of this listener
+	 * @return {@code true} if the listener was successfully added. {@code false} if it was already present
+	 */
 	public boolean addStaticListener(ChangeListener listener, ListenerPriority priority) {
 		return add(staticListeners, listener, priority);
 	}
 
+	/**
+	 * Adds the specified listener to the list of static listeners with the specified priority.
+	 * @param listener element to be added
+	 * @param priority priority of this listener
+	 * @return {@code true} if the listener was successfully added. {@code false} if it was already present
+	 */
 	public boolean addStaticListener(ChangeListener listener, int priority) {
 		return add(staticListeners, listener, priority);
 	}
 
+	/**
+	 * Removes the specified static listener from the list of listeners.
+	 * @param listener element to be removed
+	 * @return {@code true} if the listener was successfully removed. {@code false} if it was not found
+	 */
 	public boolean removeStaticListener(ChangeListener listener) {
 		return remove(staticListeners, listener);
 	}
 
+	/**
+	 * Sets the specified {@code Observable} as a parent of this {@code SettableObservable}. <i>Note: this creates only one way binding.</i>
+	 * {@link #addSubObservable(Observable)} method is recommended for setting up {@code Observable} relations. Method overridden to provide additional functionality.
+	 * @param observable parent to be added
+	 * @return {@code true} if the {@code Observable} was successfully added. {@code false} if it was already present
+	 */
 	@Override
 	protected boolean addParent(Observable observable) {
 		if(modelValue != null) {
@@ -60,6 +95,12 @@ public class SettableObservable<T extends Observable> extends SettableProperty<T
 		return super.addParent(observable);
 	}
 
+	/**
+	 * Removes the specified {@code Observable} from parents of this {@code SettableObservable}. <i>Note: this removed one way binding only.</i>
+	 * {@link #removeSubObservable(Observable)} method is recommended for removing {@code Observable} relations. Method overridden to provide additional functionality.
+	 * @param observable parent to be removed
+	 * @return {@code true} if the {@code Observable} was successfully removed. {@code false} if it was not found
+	 */
 	@Override
 	protected boolean removeParent(Observable observable) {
 		if(modelValue != null) {
@@ -68,6 +109,10 @@ public class SettableObservable<T extends Observable> extends SettableProperty<T
 		return super.removeParent(observable);
 	}
 
+	/**
+	 * Sets the {@link #modelValue} of this {@code SettableObservable}. Method overridden to provide additional functionality.
+	 * @param modelValue value to be set
+	 */
 	@Override
 	public void set(T modelValue) {
 		if(this.modelValue == modelValue) {
