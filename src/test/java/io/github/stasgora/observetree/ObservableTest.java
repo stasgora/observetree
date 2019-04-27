88d0da95859697726ca152a7cea6fced72dea9f1
Stanisław Góra <stasgora@gmail.com>
2019-04-28 00:46:26 +0200

    parent observable tests

--- src/test/java/io/github/stasgora/observetree/ObservableTest.java
+++ src/test/java/io/github/stasgora/observetree/ObservableTest.java
@@ -19,57 +19,91 @@ import org.mockito.Mockito;
 
 public class ObservableTest {
 
-	private TestObservable observable;
 	private static final int VALUE_TO_SET = 2;
 
+	private TestObservable observable;
+	private ChangeListener listener;
+
 	@Before
-	public void prepareObservables() {
+	public void prepareObjects() {
 		observable = new TestObservable();
+		listener = mockListener(() -> Assert.assertEquals(observable.value, VALUE_TO_SET));
 	}
 
 	@Test
 	public void whenManualObservableIsChanged_listenerIsCalledAndValueSet() {
-		ChangeListener listener = mockListener(() -> Assert.assertEquals(observable.value, VALUE_TO_SET));
 		observable.addListener(listener);
-
 		observable.setValue(VALUE_TO_SET);
 		observable.notifyListeners();
-		Mockito.verify(listener, Mockito.times(1)).call();
+		verifyListenerCalled(listener, 1);
 	}
 
 	@Test
 	public void whenAutomaticObservableIsChanged_listenerIsCalledAndValueSet() {
 		observable.notificationMethod = ListenerNotification.AUTOMATIC;
-		ChangeListener listener = mockListener(() -> Assert.assertEquals(observable.value, VALUE_TO_SET));
 		observable.addListener(listener);
-
 		observable.setValue(VALUE_TO_SET);
-		Mockito.verify(listener, Mockito.times(1)).call();
+		verifyListenerCalled(listener, 1);
+	}
+
+	@Test
+	public void whenRemovingListener_itIsUnregistered() {
+		observable.addListener(listener);
+		observable.removeListener(listener);
+		observable.setValue(VALUE_TO_SET);
+		observable.notifyListeners();
+		verifyListenerCalled(listener, 0);
 	}
 
 	@Test
 	public void whenAddingHighPriorityListener_itIsCalledEarlier() {
-		ChangeListener normalPriorityListener = mockListener(() -> {});
 		ChangeListener highPriorityListener = mockListener(() -> {});
-		observable.addListener(normalPriorityListener);
+		observable.addListener(listener);
 		observable.addListener(highPriorityListener, ListenerPriority.HIGH);
 
 		observable.setValue(VALUE_TO_SET);
 		observable.notifyListeners();
-		InOrder inOrder = Mockito.inOrder(normalPriorityListener, highPriorityListener);
+		InOrder inOrder = Mockito.inOrder(listener, highPriorityListener);
 		inOrder.verify(highPriorityListener, Mockito.times(1)).call();
-		inOrder.verify(normalPriorityListener, Mockito.times(1)).call();
+		inOrder.verify(listener, Mockito.times(1)).call();
+	}
+
+	@Test
+	public void whenThereIsParentObservable_itsListenerIsCalled() {
+		TestObservable parent = new TestObservable();
+		parent.addSubObservable(observable);
+		parent.addListener(listener);
+
+		observable.setValue(VALUE_TO_SET);
+		observable.notifyListeners();
+		verifyListenerCalled(listener, 1);
+	}
+
+	@Test
+	public void whenParentObservableIsRemoved_itIsUnregistered() {
+		TestObservable parent = new TestObservable();
+		parent.addSubObservable(observable);
+		parent.addListener(listener);
+		parent.removeSubObservable(observable);
+
+		observable.setValue(VALUE_TO_SET);
+		observable.notifyListeners();
+		verifyListenerCalled(listener, 0);
 	}
 
 	private ChangeListener mockListener(ChangeListener listener) {
 		return Mockito.mock(ChangeListener.class, AdditionalAnswers.delegatesTo(listener));
 	}
 
+	private void verifyListenerCalled(ChangeListener listener, int times) {
+		Mockito.verify(listener, Mockito.times(times)).call();
+	}
+
 	class TestObservable extends Observable {
 		private int value;
 
-		void setValue(int var) {
-			this.value = var;
+		void setValue(int value) {
+			this.value = value;
 			onValueChanged();
 		}
 	}
