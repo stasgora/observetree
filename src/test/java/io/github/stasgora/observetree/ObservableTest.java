34ef6dd628fe7edce396772af3f57ed25f567133
Stanisław Góra <stasgora@gmail.com>
2019-04-27 23:13:09 +0200

    listener priority test

--- src/test/java/io/github/stasgora/observetree/ObservableTest.java
+++ src/test/java/io/github/stasgora/observetree/ObservableTest.java
@@ -7,13 +7,63 @@
 
 package io.github.stasgora.observetree;
 
+import io.github.stasgora.observetree.enums.ListenerNotification;
+import io.github.stasgora.observetree.enums.ListenerPriority;
 import io.github.stasgora.observetree.listener.ChangeListener;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.AdditionalAnswers;
+import org.mockito.InOrder;
 import org.mockito.Mockito;
 
 public class ObservableTest {
+
+	private TestObservable observable;
+	private static final int VALUE_TO_SET = 2;
+
+	@Before
+	public void prepareObservables() {
+		observable = new TestObservable();
+	}
+
+	@Test
+	public void whenManualObservableIsChanged_listenerIsCalledAndValueSet() {
+		ChangeListener listener = mockListener(() -> Assert.assertEquals(observable.value, VALUE_TO_SET));
+		observable.addListener(listener);
+
+		observable.setValue(VALUE_TO_SET);
+		observable.notifyListeners();
+		Mockito.verify(listener, Mockito.times(1)).call();
+	}
+
+	@Test
+	public void whenAutomaticObservableIsChanged_listenerIsCalledAndValueSet() {
+		observable.notificationMethod = ListenerNotification.AUTOMATIC;
+		ChangeListener listener = mockListener(() -> Assert.assertEquals(observable.value, VALUE_TO_SET));
+		observable.addListener(listener);
+
+		observable.setValue(VALUE_TO_SET);
+		Mockito.verify(listener, Mockito.times(1)).call();
+	}
+
+	@Test
+	public void whenAddingHighPriorityListener_itIsCalledEarlier() {
+		ChangeListener normalPriorityListener = mockListener(() -> {});
+		ChangeListener highPriorityListener = mockListener(() -> {});
+		observable.addListener(normalPriorityListener);
+		observable.addListener(highPriorityListener, ListenerPriority.HIGH);
+
+		observable.setValue(VALUE_TO_SET);
+		observable.notifyListeners();
+		InOrder inOrder = Mockito.inOrder(normalPriorityListener, highPriorityListener);
+		inOrder.verify(highPriorityListener, Mockito.times(1)).call();
+		inOrder.verify(normalPriorityListener, Mockito.times(1)).call();
+	}
+
+	private ChangeListener mockListener(ChangeListener listener) {
+		return Mockito.mock(ChangeListener.class, AdditionalAnswers.delegatesTo(listener));
+	}
 
 	class TestObservable extends Observable {
 		private int value;
@@ -22,22 +72,6 @@ public class ObservableTest {
 			this.value = var;
 			onValueChanged();
 		}
-	}
-
-	@Test
-	public void whenObservableIsChanged_listenerIsCalledAndValueSet() {
-		TestObservable observable = new TestObservable();
-		int valueToSet = 2;
-		ChangeListener listener = mockListener(() -> Assert.assertEquals(observable.value, valueToSet));
-		observable.addListener(listener);
-
-		observable.setValue(valueToSet);
-		observable.notifyListeners();
-		Mockito.verify(listener, Mockito.times(1)).call();
-	}
-
-	private ChangeListener mockListener(ChangeListener listener) {
-		return Mockito.mock(ChangeListener.class, AdditionalAnswers.delegatesTo(listener));
 	}
 
 }
