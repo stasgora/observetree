c51a7b7ff5944e579bdd2e85b92e786fc3602620
Stanisław Góra <stasgora@gmail.com>
2019-09-16 23:07:42 +0200

    Additional settable property tests

--- src/test/java/io/github/stasgora/observetree/settable/SettablePropertyTest.java
+++ src/test/java/io/github/stasgora/observetree/settable/SettablePropertyTest.java
@@ -5,6 +5,9 @@ import io.github.stasgora.observetree.TestBase;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class SettablePropertyTest extends TestBase {
 	private SettableProperty<Integer> settableInt;
 
@@ -22,5 +25,35 @@ public class SettablePropertyTest extends TestBase {
 		settableInt.notifyListeners();
 
 		verifyListenerCalled(listener, 1);
+	}
+
+	@Test
+	public void whenValueIsModified_itIsMarkedAsChanged() {
+		settableInt.addListener(listener);
+
+		settableInt.modify(val -> VALUE_TO_SET);
+
+		assertEquals((int) settableInt.get(), VALUE_TO_SET);
+		assertTrue(settableInt.isValueChanged());
+	}
+
+	@Test
+	public void whenSettableObjectIsSetWithNotify_theListenersGetsCalled() {
+		settableInt.addListener(listener);
+
+		settableInt.setAndNotify(VALUE_TO_SET);
+
+		verifyListenerCalled(listener, 1);
+	}
+
+	@Test
+	public void whenSettingDefaultValue_itCanBeRestored() {
+		settableInt.set(VALUE_TO_SET);
+		settableInt.saveAsDefaultValue();
+		settableInt.set(0);
+
+		assertEquals((int) settableInt.getDefaultValue(), VALUE_TO_SET);
+		settableInt.resetToDefaultValue();
+		assertEquals((int) settableInt.get(), VALUE_TO_SET);
 	}
 }
